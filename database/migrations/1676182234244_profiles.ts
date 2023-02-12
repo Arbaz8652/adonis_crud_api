e56853d712dca149950eb5654ec8b4846a85ec16
Arbaz Pathan <arbaz.pathan.22.1@mountblue.tech>
2023-02-12 11:46:55 +0530

    migration for profile table done

---
+++ database/migrations/1676182234244_profiles.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Profiles extends BaseSchema {
+  protected tableName = 'profiles'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.string('full_name').notNullable()
+      table.string('email').notNullable()
+      table.string('gender').notNullable()
+      table.string('contact_number').notNullable()
+      table.string('date_of_birth').notNullable()
+      table.timestamps(true,true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
