fe45cfffd3a0e38f26ce0b93e09bd8bdbd28063f
Arbaz Pathan <arbaz.pathan.22.1@mountblue.tech>
2023-02-12 14:11:30 +0530

    createUser() methode with validation and error handling done

--- app/Controllers/Http/ProfilesController.ts
+++ app/Controllers/Http/ProfilesController.ts
@@ -1,32 +1,34 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Profile from 'App/Models/Profile'
+import ProfileValidator from 'App/Validators/ProfileValidator'
 
 export default class ProfilesController {
 
 
-  public async createUser({request, response}:HttpContextContract){
+  public async createUser({request, response,auth}:HttpContextContract){
 
-    const user = await Profile.create({
-      email:request.input('email'),
-      dateOfBirth:request.input('password')  
-    })
+    const userId = auth.use('api').user!.id
+    const email = auth.use('api').user!.email
+    const payload = await request.validate(ProfileValidator)
 
-    return response.status(200).send(user)    
+    try{
+      const newProfile = await Profile.create({
+        userId:userId,
+        fullName:payload.full_name,
+        email:email,
+        gender:payload.gender,
+        contactNumber:payload.contact_number,
+        dateOfBirth: payload.date_of_birth
+      })
+      response.created(newProfile)
+
+    }catch(e){
+      response.status(409).send({
+        message:"For one user only one profile can be made",
+        error:e
+      })
+    }
   }
 
-
-  public async create({request, response}:HttpContextContract){
-
-    const newProfile = await Profile.create({
-      userId:1,
-      fullName:request.input('full_name'),
-      email:"asrv@gmail.com",
-      gender:request.input("gender"),
-      contactNumber:request.input('contact_number'),
-      dateOfBirth:request.input('dop')  
-    })
-
-    return response.status(200).send(newProfile)    
-  }
 
 }
