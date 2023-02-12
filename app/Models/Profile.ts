17417f2a57a379de03a94c3e58da95fd1eafc35e
Arbaz Pathan <arbaz.pathan.22.1@mountblue.tech>
2023-02-12 11:25:59 +0530

    Profile Models Column defined

---
+++ app/Models/Profile.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Profile extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId:number
+
+  @column()
+  public fullName:string
+
+  @column()
+  public email:string
+
+  @column()
+  public gender:string
+
+  @column()
+  public dateOfBirth:DateTime
+
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
