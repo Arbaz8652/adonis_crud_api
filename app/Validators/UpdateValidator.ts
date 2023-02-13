3f3d78195135c3c6cdc8278f2495f38d31328639
Arbaz Pathan <arbaz.pathan.22.1@mountblue.tech>
2023-02-13 16:18:41 +0530

    Custome error messages are added

--- app/Validators/UpdateValidator.ts
+++ app/Validators/UpdateValidator.ts
@@ -48,5 +48,13 @@ export default class UpdateValidator {
    * }
    *
    */
-  public messages: CustomMessages = {}
+  public messages: CustomMessages = {
+    'full_name.maxLength':"full_name cannot have more than 30 characters",
+    'full_name.minLength':`{{field}} must contain atleast 3 characters`,
+    'full_name.regex':"full_name cannot contain special characters or numbers",
+    'contact_number.regex':"contact_number cannot contain special characters",
+    'contact_number.unique':"This number is already registered!",
+    'gender.enum':"Gender should be one of [MALE,FEMALE]",
+    'date_of_birth.date':"date_of_birth should a valid date and format = DD-MM-YYYY",
+  }
 }
